188637c864f04c84e9e760aca91d1059f58e998d
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-06-16 00:24:44 +0530

    Added 2 new projects

--- frontend/src/Components/Projects/Projects.jsx
+++ frontend/src/Components/Projects/Projects.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import './Projects.css'
 import img1 from '../Assets/experince-img.jpg'
+import img2 from '../../assets/glimps1.png';
+import img3 from '../../assets/notesphere.png';
 
 
 const Projects = () => {
     return (
         <div id="projects" className='projects-container'>
-            <hr/>
+            <hr />
             <h2>- MY PROJECTS - </h2>
             <div className='projects-card-container'>
                 <div className='project-card'>
                     <img
-                        src={img1}
+                        src={img2}
                         alt="Static Card"
                         className="static-card-image"
                     />
                     <div className="static-card-content">
-                        <h3 className="static-card-title">EasyBook <a href="https://easybook.onrender.com/listings">Live</a></h3>
+                        <h3 className="static-card-title">MockInter <a href="https://mock-inter-fzjn.vercel.app/sign-in">Live</a></h3>
                         <p className="static-card-description">
-                        EasyBook offers a seamless interface where users can not only discover a diverse array of listings.
+                            Designed and deployed an AI-driven mock interview platform using Next.js, Node.js, and OpenAI API.
+                        </p>
+                    </div>
+                </div>
+                <div className='project-card'>
+                    <img
+                        src={img3}
+                        alt="Static Card"
+                        className="static-card-image"
+                    />
+                    <div className="static-card-content">
+                        <h3 className="static-card-title">NoteSphere <a href="https://www.linkedin.com/posts/anjali-daharwal_nextjs-firebase-liveblocks-activity-7311097326201143296-j6kq?utm_source=share&utm_medium=member_desktop&rcm=ACoAAEEWEEkB2_tlSG7pdjpvgdIVs6-ObNTirec">Live</a></h3>
+                        <p className="static-card-description">
+                            Developed a Notion-inspired real-time collaboration app using Next.js (SSR), Firebase (Realtime DB), and Tailwind CSS,
+                            increasing productivity by 80%.
                         </p>
                     </div>
                 </div>
@@ -30,9 +46,9 @@ const Projects = () => {
                         className="static-card-image"
                     />
                     <div className="static-card-content">
-                        <h3 className="static-card-title">ShopSpot <a href="https://shopspots1.netlify.app/">Live</a></h3>
+                        <h3 className="static-card-title">EasyBook <a href="https://easybook.onrender.com/listings">Live</a></h3>
                         <p className="static-card-description">
-                            Your One stop for shopping, gives seamless user experience. 
+                            EasyBook offers a seamless interface where users can not only discover a diverse array of listings.
                         </p>
                     </div>
                 </div>
@@ -46,7 +62,7 @@ const Projects = () => {
                     <div className="static-card-content">
                         <h3 className="static-card-title">RefreshLine <a href="https://github.com/anjali012005/DynamicQuote-Generator">Live</a></h3>
                         <p className="static-card-description">
-                        Embrace the power of spontaneous inspiration! Explore a repository of wisdom with every refresh.
+                            Embrace the power of spontaneous inspiration! Explore a repository of wisdom with every refresh.
                         </p>
                     </div>
                 </div>
@@ -75,8 +91,8 @@ const Projects = () => {
                         <h3 className="static-card-title">DoTask <a href="https://github.com/anjali012005/TODO">Live</a></h3>
                         <p className="static-card-description">
                             It allows user to add, delete, update their daily tasks.
-                        </p>    
-                    </div>  
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
